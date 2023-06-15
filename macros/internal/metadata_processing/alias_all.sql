03ea489388e2322805e4e8faa4896e6076c2b830
Ruben van Zessen <ruben.van.zessen@knab.nl>
2023-06-15 16:58:22 +0200

    Add Redshift multi-active sat

--- macros/internal/metadata_processing/alias_all.sql
+++ macros/internal/metadata_processing/alias_all.sql
@@ -2,27 +2,29 @@
 
     {{- adapter.dispatch('alias_all', 'datavault4dbt')(columns=columns, prefix=prefix) -}}
 
-{%- endmacro %}
+{%- endmacro -%}
 
 {%- macro default__alias_all(columns, prefix) -%}
 
 {%- if datavault4dbt.is_list(columns) -%}
 
     {%- for column in columns -%}
-        {{ datavault4dbt.alias(alias_config=column, prefix=prefix) }}
-        {% if not loop.last -%} , {% endif -%}
+        {{- datavault4dbt.alias(alias_config=column, prefix=prefix) -}}
+        {%- if not loop.last -%}
+            {{- ",\n" -}}
+        {%- endif -%}
     {%- endfor -%}
 
 {%- elif columns is string -%}
 
-{{ datavault4dbt.alias(alias_config=columns, prefix=prefix) }}
+    {{- datavault4dbt.alias(alias_config=columns, prefix=prefix) -}}
 
 {%- else -%}
 
     {%- if execute -%}
-        {{ exceptions.raise_compiler_error("Invalid columns object provided. Must be a list or a string.") }}
+        {{- exceptions.raise_compiler_error("Invalid columns object provided. Must be a list or a string.") -}}
     {%- endif %}
 
-{%- endif %}
+{%- endif -%}
 
-{%- endmacro -%}
+{%- endmacro %}
